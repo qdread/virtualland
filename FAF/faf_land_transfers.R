23eab6a7d5028fa4f74983c6275020c389a401c7
qdread <qread@sesync.org>
2020-11-18 20:36:57 -0500

    update faf_land_transfers with annual vs permanent

--- FAF/faf_land_transfers.R
+++ FAF/faf_land_transfers.R
@@ -2,6 +2,7 @@
 # QDR / FWE / 19 May 2020
 
 # Modified 15 Sep 2020: Include foreign-origin transfers.
+# Modified 18 Nov 2020: Include annual& permanent cropland separately.
 
 # Load data ---------------------------------------------------------------
 
@@ -33,8 +34,8 @@ vlt_foreign <- read_csv(file.path(fp_out, 'foreign_VLT_by_region.csv'))
 
 nass_bea_land <- nass_bea_land %>%
   left_join(faf_lookup) %>%
-  select(Code, FAF_Region, BEA_code, cropland_normalized, pastureland_normalized) %>%
-  setNames(c('FAF_Code', 'FAF_Region', 'BEA_Code', 'cropland', 'pastureland'))
+  select(Code, FAF_Region, BEA_code, annual_cropland_normalized, permanent_cropland_normalized, pastureland_normalized) %>%
+  setNames(c('FAF_Code', 'FAF_Region', 'BEA_Code', 'annual_cropland', 'permanent_cropland', 'pastureland'))
 
 # Join FAF with NASS land values
 faf_by_bea <- faf_by_bea %>%
@@ -45,15 +46,18 @@ faf_by_bea <- faf_by_bea %>%
 # Separate shipments with foreign origin (trade type 2) 
 
 # Divide the virtual crop and pasture transfers evenly based on foreign transfers of agricultural products.
-crop_codes <- sprintf('%02d', c(2,3,6,7,8,9))
+# These codes are relatively crude division between annual and permanent crops.
+annual_crop_codes <- sprintf('%02d', c(2,6,8,9))
+permanent_crop_codes <- sprintf('%02d', c(3,7))
 pasture_codes <- sprintf('%02d', c(1,4,5))
 
 faf_by_bea_foreign <- faf_by_bea %>%
   filter(trade_type == '2') %>%
-  mutate(land_type = case_when(SCTG_Code %in% crop_codes ~ 'cropland',
+  mutate(land_type = case_when(SCTG_Code %in% annual_crop_codes ~ 'annual_cropland',
+                               SCTG_Code %in% permanent_crop_codes ~ 'permanent_cropland',
                                SCTG_Code %in% pasture_codes ~ 'pastureland',
                                TRUE ~ 'none')) %>%
-  filter(land_type %in% c('cropland', 'pastureland')) %>%
+  filter(land_type %in% c('annual_cropland', 'permanent_cropland', 'pastureland')) %>%
   group_by(fr_orig, dms_orig, dms_dest, fr_dest, fr_inmode, fr_outmode, land_type) %>%
   summarize(tons_2012 = sum(tons_2012), 
             value_2012 = sum(value_2012),
@@ -69,19 +73,22 @@ faf_by_bea_foreign <- faf_by_bea_foreign %>%
   left_join(vlt_foreign %>% mutate(FAF_foreign_region_code = factor(FAF_foreign_region_code)), by = c('fr_orig' = 'FAF_foreign_region_code')) %>%
   ungroup %>%
   group_by(fr_orig) %>%
-  mutate(cropland_proportion = cropland / sum(cropland, na.rm = TRUE),
+  mutate(annual_cropland_proportion = annual_cropland / sum(annual_cropland, na.rm = TRUE),
+         permanent_cropland_proportion = permanent_cropland / sum(permanent_cropland, na.rm = TRUE),
          pastureland_proportion = pastureland / sum(pastureland, na.rm = TRUE)) %>%
   ungroup %>%
-  mutate(cropland_flow = VLT_crop * cropland_proportion,
+  mutate(annual_cropland_flow = VLT_crop * annual_cropland_proportion,
+         permanent_cropland_flow = VLT_crop * permanent_cropland_proportion,
          pastureland_flow = VLT_pasture * pastureland_proportion)
 
 # Convert domestic by weight.
 faf_by_bea <- faf_by_bea %>%
-  filter(trade_type != '2', !is.na(cropland), !is.na(pastureland)) %>%
+  filter(trade_type != '2', !is.na(annual_cropland), !is.na(permanent_cropland), !is.na(pastureland)) %>%
   group_by(dms_orig) %>%
   mutate(tons_proportion = tons_2012 / sum(tons_2012, na.rm = TRUE)) %>%
   ungroup %>%
-  mutate(cropland_flow = tons_proportion * cropland,
+  mutate(annual_cropland_flow = tons_proportion * annual_cropland,
+         permanent_cropland_flow = tons_proportion * permanent_cropland,
          pastureland_flow = tons_proportion * pastureland)
 
 # Convert units of domestic land transfers from acres to square km
@@ -92,7 +99,8 @@ faf_by_bea <- faf_by_bea %>%
 
 # Convert units of foreign import transfers from hectares to square km
 faf_by_bea_foreign <- faf_by_bea_foreign %>%
-  mutate(cropland_flow = cropland_flow * 0.01,
+  mutate(annual_cropland_flow = annual_cropland_flow * 0.01,
+         permanent_cropland_flow = permanent_cropland_flow * 0.01,
          pastureland_flow = pastureland_flow * 0.01)
 
 # Calculate summary totals ------------------------------------------------
@@ -101,46 +109,57 @@ faf_by_bea_foreign <- faf_by_bea_foreign %>%
 land_outbound <- faf_by_bea %>%
   filter(trade_type == 1, dms_orig != dms_dest) %>%
   group_by(dms_orig) %>%
-  summarize(cropland_flow = sum(cropland_flow), pastureland_flow = sum(pastureland_flow)) %>%
-  add_row(dms_orig = '111', cropland_flow = 0, pastureland_flow = 0) # Washington DC
+  summarize(annual_cropland_flow = sum(annual_cropland_flow),
+            permanent_cropland_flow = sum(permanent_cropland_flow),
+            pastureland_flow = sum(pastureland_flow)) %>%
+  add_row(dms_orig = '111', annual_cropland_flow = 0, permanent_cropland_flow = 0, pastureland_flow = 0) # Washington DC
 
 land_inbound <- faf_by_bea %>%
   filter(trade_type == 1, dms_orig != dms_dest) %>%
   group_by(dms_dest) %>%
-  summarize(cropland_flow = sum(cropland_flow), pastureland_flow = sum(pastureland_flow)) 
+  summarize(annual_cropland_flow = sum(annual_cropland_flow),
+            permanent_cropland_flow = sum(permanent_cropland_flow),
+            pastureland_flow = sum(pastureland_flow)) 
 
 land_netdomestic <- left_join(land_outbound, land_inbound, by = c('dms_orig' = 'dms_dest')) %>%
-  mutate(cropland_flow = cropland_flow.y - cropland_flow.x,
+  mutate(annual_cropland_flow = annual_cropland_flow.y - annual_cropland_flow.x,
+         permanent_cropland_flow = permanent_cropland_flow.y - permanent_cropland_flow.x,
          pastureland_flow = pastureland_flow.y - pastureland_flow.x) %>%
-  select(dms_orig, cropland_flow, pastureland_flow) %>%
+  select(dms_orig, annual_cropland_flow, permanent_cropland_flow, pastureland_flow) %>%
   rename(region = dms_orig)
 
 # Foreign exports
 land_export <- faf_by_bea %>%
   filter(trade_type == 3) %>%
   group_by(dms_orig) %>%
-  summarize(cropland_flow = sum(cropland_flow), pastureland_flow = sum(pastureland_flow)) %>%
-  add_row(dms_orig = '111', cropland_flow = 0, pastureland_flow = 0) # Washington DC
+  summarize(annual_cropland_flow = sum(annual_cropland_flow),
+            permanent_cropland_flow = sum(permanent_cropland_flow),
+            pastureland_flow = sum(pastureland_flow)) %>%
+  add_row(dms_orig = '111', annual_cropland_flow = 0, permanent_cropland_flow = 0, pastureland_flow = 0) # Washington DC
 
 # Foreign imports
 land_import <- faf_by_bea_foreign %>%
   group_by(dms_dest) %>%
-  summarize(cropland_flow = sum(cropland_flow, na.rm = TRUE), pastureland_flow = sum(pastureland_flow, na.rm = TRUE))
+  summarize(annual_cropland_flow = sum(annual_cropland_flow, na.rm = TRUE),
+            permanent_cropland_flow = sum(permanent_cropland_flow, na.rm = TRUE),
+            pastureland_flow = sum(pastureland_flow, na.rm = TRUE))
 
 # Net foreign import-export balance
 land_netforeign <- left_join(land_export, land_import, by = c('dms_orig' = 'dms_dest')) %>%
-  mutate(cropland_flow = cropland_flow.y - cropland_flow.x,
+  mutate(annual_cropland_flow = annual_cropland_flow.y - annual_cropland_flow.x,
+         permanent_cropland_flow = permanent_cropland_flow.y - permanent_cropland_flow.x,
          pastureland_flow = pastureland_flow.y - pastureland_flow.x) %>%
-  select(dms_orig, cropland_flow, pastureland_flow) %>%
+  select(dms_orig, annual_cropland_flow, permanent_cropland_flow, pastureland_flow) %>%
   rename(region = dms_orig)
 
 
 # Net land transfers, including both domestic and foreign.
 
 land_netall <- left_join(land_netdomestic, land_netforeign, by = 'region') %>%
-  mutate(cropland_flow = cropland_flow.y + cropland_flow.x,
+  mutate(annual_cropland_flow = annual_cropland_flow.y + annual_cropland_flow.x,
+         permanent_cropland_flow = permanent_cropland_flow.y + permanent_cropland_flow.x,
          pastureland_flow = pastureland_flow.y + pastureland_flow.x) %>%
-  select(region, cropland_flow, pastureland_flow)
+  select(region, annual_cropland_flow, permanent_cropland_flow, pastureland_flow)
 
 # Write outputs
 # Write faf_by_bea with the flows to an object
