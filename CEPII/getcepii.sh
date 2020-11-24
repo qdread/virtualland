186f5c8599032211dfbc26b1a318f1924120e3c4
Quentin Read <quentin.read@gmail.com>
2020-11-24 12:42:07 -0500

    script to download cepii data

---
+++ CEPII/getcepii.sh
@@ -0,0 +1,11 @@
+# Download tradeprod
+
+cd /nfs/qread-data/raw_data/commodity_flows/CEPII
+
+wget http://www.cepii.fr/DATA_DOWNLOAD/tradeprod/Prod_cepii8006_CSV.rar
+wget http://www.cepii.fr/DATA_DOWNLOAD/tradeprod/Trade_cepii8006_CSV.rar
+wget http://www.cepii.fr/DATA_DOWNLOAD/tradeprod/Tar_cepii8901_CSV.rar
+
+unrar x Prod_cepii8006_CSV.rar
+unrar x Trade_cepii8006_CSV.rar
+unrar x Tar_cepii8901_CSV.rar
