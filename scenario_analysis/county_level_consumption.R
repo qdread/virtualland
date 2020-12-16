4a4c705923f3887637552cc888842d6da8004829
Quentin Read <quentin.read@gmail.com>
2020-12-16 15:44:24 -0500

    Start to create land exchange satellite tables for each state

--- scenario_analysis/county_level_consumption.R
+++ scenario_analysis/county_level_consumption.R
@@ -2,6 +2,9 @@
 # Find by multiplying the total PCE for each BEA category from the input-output table (2012) times the percentage of personal income
 # that each county consists of, out of the USA total.
 
+
+# Final consumer expenditures ---------------------------------------------
+
 # Read the county-level income data from Lin et al. (provided by Landon 9 Dec 2020)
 
 library(tidyverse)
@@ -37,3 +40,43 @@ county_consumption2012 <- pce2012 %*% t(county_income_norm2012) # 389 x 3142 mat
 county_consumption2012 <- cbind(BEA_code = names(pce2012), as.data.frame(county_consumption2012))
 
 write_csv(county_consumption2012, file.path(fp, 'cfs_io_analysis/county_consumption2012.csv'))                    
+
+
+# Direct and indirect demand resulting from consumer expenditures ---------
+
+# We can use the PCE vector for each county as a final demand vector.
+# Multiply the direct requirements coefficients matrix * this vector.
+# The product will be the total direct and indirect demand for each county.
+
+county_consumption2012 <- read_csv(file.path(fp, 'cfs_io_analysis/county_consumption2012.csv'))
+
+fp_useeio <- file.path(ifelse(is_local, '~/Documents/GitHub/foodwaste', '~'), 'USEEIO/useeiopy/Model Builds/USEEIO2012')
+
+drc2012 <- read.csv(file.path(fp_useeio, 'USEEIO2012_DRC.csv'), row.names = 1, check.names = FALSE) # Use this to get row names
+
+# Convert DRC2012 row and column names to the six digit code with uppercase letters
+dimnames(drc2012) <- map(dimnames(drc2012), ~ toupper(substr(., 1, 6)))
+
+# There is one extra column in county consumption not present in the DRC2012
+setdiff(county_consumption2012$BEA_code, dimnames(drc2012)[[1]])
+# This is the scrap column. Ignore this column and ensure the vector and matrix are sorted the same way.
+
+all(dimnames(drc2012)[[1]] == dimnames(drc2012)[[2]]) # Already sorted OK.
+
+county_consumption2012 <- filter(county_consumption2012, BEA_code %in% dimnames(drc2012)[[1]])
+
+all(county_consumption2012$BEA_code == dimnames(drc2012)[[1]]) # Already sorted OK
+
+# Columnwise, multiply the matrix drc2012 times the consumption vectors for each county.
+
+drc2012_mat <- as.matrix(drc2012)
+
+county_indirectdemand2012 <- apply(county_consumption2012[, -1], 2, function(f) drc2012_mat %*% f)
+
+county_totaldemand2012 <- county_consumption2012[,-1] + county_indirectdemand2012
+
+county_indirectdemand2012 <- data.frame(BEA_code = county_consumption2012$BEA_code, county_indirectdemand2012, check.names = FALSE)
+county_totaldemand2012 <- data.frame(BEA_code = county_consumption2012$BEA_code, county_totaldemand2012, check.names = FALSE)
+
+write_csv(county_indirectdemand2012, file.path(fp, 'cfs_io_analysis/county_indirectdemand2012.csv'))    
+write_csv(county_totaldemand2012, file.path(fp, 'cfs_io_analysis/county_totaldemand2012.csv'))    
--- scenario_analysis/footprint_estimate_v2.R
+++ scenario_analysis/footprint_estimate_v2.R
@@ -11,3 +11,30 @@
 # We also have the land area of production in each county (NLCD sums).
 # Scale the production and consumption land areas so they add up to the same.
 # Next, use the FAF dataset (harmonization between BEA and SCTG codes) to work backwards and get 
+
+
+# We have the county-level direct and indirect consumption. (see county_level_consumption.R)
+# We have the land exchanges. (see eeio_landdata.R and impute_exchanges.R)
+
+
+# Load data ---------------------------------------------------------------
+
+library(tidyverse)
+
+is_local <- dir.exists('Q:/')
+
+fp <- ifelse(is_local, 'Q:', '/nfs/qread-data')
+fp_out <- file.path(fp, 'cfs_io_analysis')
+
+
+land_exchanges <- read_csv(file.path(fp, 'raw_data/IO_tables/output_csvs/land_exchanges_bytype.csv'))
+land_exch_imputed <- read_csv(file.path(fp_out, 'land_imputed_exchanges_wide.csv'))
+
+# Convert land exchanges to a satellite table -----------------------------
+
+# N rows = 7 (number of land use types in the land_exchanges data)
+# N columns = 388 (BEA codes)
+# Need to create a separate satellite table for each state. 
+# (Each matrix will be relatively sparse, with around 22K nonzero entries total out of a potential 136K.)
+# Then, multiply the appropriate state's satellite table by each county's vector
+
