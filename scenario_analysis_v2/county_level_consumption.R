31f392f3a481ab0f2142eaa54f7e2f283f0adba0
qdread <qread@sesync.org>
2021-01-06 21:51:58 -0500

    update county consumption script to do all 20 scenarii

--- scenario_analysis_v2/county_level_consumption.R
+++ scenario_analysis_v2/county_level_consumption.R
@@ -2,6 +2,7 @@
 # Find by multiplying the total PCE for each BEA category from the input-output table (2012) times the percentage of personal income
 # that each county consists of, out of the USA total.
 
+# Modification 06 Jan 2021: add the alternative scenarios as well as the baseline.
 
 # Final consumer expenditures ---------------------------------------------
 
@@ -10,11 +11,8 @@
 library(tidyverse)
 library(readxl)
 
-is_local <- dir.exists('Q:/')
-
-fp <- ifelse(is_local, 'Q:', '/nfs/qread-data')
-fp_lin <- file.path(fp, 'raw_data/commodity_flows/Lin_supp_info')
-fp_bea <- file.path(fp, 'raw_data/BEA/formatted')
+fp_lin <- 'data/raw_data/commodity_flows/Lin_supp_info'
+fp_bea <- 'data/raw_data/BEA/formatted'
 
 county_income <- read_xlsx(file.path(fp_lin, 'County Personal Income.xlsx'), sheet = 'Total County Income', skip = 5)
 
@@ -31,16 +29,46 @@ use2012 <- read_csv(file.path(fp_bea, 'use2012.csv'))
 # F01000 is the code for personal consumption expenditures.
 pce2012 <- setNames(use2012$F01000[1:389], use2012$X1[1:389])
 
+
+# Alternative scenarios ---------------------------------------------------
+
+# Get multiplicative factors for each of the alternative scenarios
+scenario_factors_bea <- read_csv('data/cfs_io_analysis/bea_consumption_factors_diet_waste_scenarios.csv')
+
+# Multiply the appropriate BEA code by its production factor for each scenario.
+# Only the 36 food BEA codes will be multiplied, so create an expanded vector for each scenario with 1 in all other elements.
+
+expand_vec <- function(BEA_389_code, value) {
+  vec <- setNames(rep(1, length(pce2012)), names(pce2012))
+  vec[BEA_389_code] <- value
+  vec
+}
+
+scenario_vectors_bea <- scenario_factors_bea %>%
+  pivot_longer(-c(BEA_389_code, BEA_389_def), names_to = 'scenario') %>%
+  nest(data = c(BEA_389_code, BEA_389_def, value)) %>%
+  mutate(consumption_factor = map(data, ~ expand_vec(.$BEA_389_code, .$value)))
+
 # Cross product of PCE 2012 vector and the normalized county income 2012 vector
 county_income_norm2012 <- setNames(county_income$total_income_2012/sum(county_income$total_income_2012), county_income$FIPS)
 
-county_consumption2012 <- pce2012 %*% t(county_income_norm2012) # 389 x 3143 matrix, each row is a good and each column a county.
+# County consumption for each scenario.
+county_consumption2012 <- scenario_vectors_bea %>%
+  select(-data) %>%
+  mutate(county_consumption = map(consumption_factor, ~ . %*% t(county_income_norm2012)))
+# Each element of the consumption list is a 389 x 3143 matrix, each row is a good and each column a county.
+# It represents the consumption in each of the 20 scenarios.
 # Units are million USD.
 
-# Add column for the BEA code
-county_consumption2012 <- cbind(BEA_code = names(pce2012), as.data.frame(county_consumption2012))
+# Add column for the BEA code and concatenate the list of matrices into a data frame.
+county_consumption2012_df <- county_consumption2012 %>%
+  select(-consumption_factor) %>%
+  mutate(county_consumption = map(county_consumption, as_tibble)) %>%
+  unnest 
 
-write_csv(county_consumption2012, file.path(fp, 'cfs_io_analysis/county_consumption2012.csv'))                    
+county_consumption2012_df <- tibble(BEA_code = rep(names(pce2012), nrow(county_consumption2012)), county_consumption2012_df)
+
+write_csv(county_consumption2012_df, 'data/cfs_io_analysis/county_consumption2012_allscenarios.csv')
 
 
 # Direct and indirect demand resulting from consumer expenditures ---------
@@ -49,35 +77,43 @@ write_csv(county_consumption2012, file.path(fp, 'cfs_io_analysis/county_consumpt
 # Multiply the direct requirements coefficients matrix * this vector.
 # The product will be the total direct and indirect demand for each county.
 
-county_consumption2012 <- read_csv(file.path(fp, 'cfs_io_analysis/county_consumption2012.csv'))
-
-fp_useeio <- file.path(ifelse(is_local, '~/Documents/GitHub/foodwaste', '~'), 'USEEIO/useeiopy/Model Builds/USEEIO2012')
+fp_useeio <- file.path(ifelse(dir.exists('Q:/'), '~/Documents/GitHub/foodwaste', '~'), 'USEEIO/useeiopy/Model Builds/USEEIO2012')
 
 drc2012 <- read.csv(file.path(fp_useeio, 'USEEIO2012_DRC.csv'), row.names = 1, check.names = FALSE) # Use this to get row names
 
 # Convert DRC2012 row and column names to the six digit code with uppercase letters
 dimnames(drc2012) <- map(dimnames(drc2012), ~ toupper(substr(., 1, 6)))
 
+
 # There is one extra column in county consumption not present in the DRC2012
-setdiff(county_consumption2012$BEA_code, dimnames(drc2012)[[1]])
+setdiff(names(pce2012), dimnames(drc2012)[[1]])
 # This is the scrap column. Ignore this column and ensure the vector and matrix are sorted the same way.
 
 all(dimnames(drc2012)[[1]] == dimnames(drc2012)[[2]]) # Already sorted OK.
 
-county_consumption2012 <- filter(county_consumption2012, BEA_code %in% dimnames(drc2012)[[1]])
+# Remove the scrap category
+county_consumption2012 <- filter(county_consumption2012_df, BEA_code %in% dimnames(drc2012)[[1]])
 
-all(county_consumption2012$BEA_code == dimnames(drc2012)[[1]]) # Already sorted OK
-
-# Columnwise, multiply the matrix drc2012 times the consumption vectors for each county.
+all(county_consumption2012$BEA_code[1:388] == dimnames(drc2012)[[1]]) # Already sorted OK
 
 drc2012_mat <- as.matrix(drc2012)
 
-county_indirectdemand2012 <- apply(county_consumption2012[, -1], 2, function(f) drc2012_mat %*% f)
+# Convert county consumption data frame back to a list column of matrices.
+county_demand2012 <- county_consumption2012 %>%
+  group_by(scenario) %>%
+  nest(directdemand = -scenario)
 
-county_totaldemand2012 <- county_consumption2012[,-1] + county_indirectdemand2012
+# Columnwise, multiply the matrix drc2012 times the consumption vectors for each county.
+# Then sum indirect and total demand.
+county_demand2012 <- county_demand2012 %>%
+  mutate(directdemand = map(directdemand, ~ as.matrix(.x[, -1]))) %>%
+  mutate(indirectdemand = map(directdemand, ~ apply(.x, 2, function(f) drc2012_mat %*% f))) %>%
+  mutate(totaldemand = map2(directdemand, indirectdemand, ~ .x + .y))
 
-county_indirectdemand2012 <- data.frame(BEA_code = county_consumption2012$BEA_code, county_indirectdemand2012, check.names = FALSE)
-county_totaldemand2012 <- data.frame(BEA_code = county_consumption2012$BEA_code, county_totaldemand2012, check.names = FALSE)
+# Combine everything into a single data frame for each demand type.
 
-write_csv(county_indirectdemand2012, file.path(fp, 'cfs_io_analysis/county_indirectdemand2012.csv'))    
-write_csv(county_totaldemand2012, file.path(fp, 'cfs_io_analysis/county_totaldemand2012.csv'))    
+county_indirectdemand2012 <- cbind(county_consumption2012[,c('BEA_code', 'scenario')], do.call(rbind, county_demand2012$indirectdemand))
+county_totaldemand2012 <- cbind(county_consumption2012[,c('BEA_code', 'scenario')], do.call(rbind, county_demand2012$totaldemand))
+
+write_csv(county_indirectdemand2012, 'data/cfs_io_analysis/county_indirectdemand2012_allscenarios.csv')  
+write_csv(county_totaldemand2012, 'data/cfs_io_analysis/county_totaldemand2012_allscenarios.csv')
