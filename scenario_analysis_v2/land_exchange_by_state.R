b065944636ae1856d9e3768befb86baf7adfe52a
qdread <qread@sesync.org>
2021-01-08 21:51:23 -0500

    Update land footprint calc with corrected exchanges

--- scenario_analysis_v2/land_exchange_by_state.R
+++ scenario_analysis_v2/land_exchange_by_state.R
@@ -35,4 +35,21 @@ nass_bea <- nass_bea %>%
 # Reshape data to desired format ------------------------------------------
 
 # For each state we need a 3x10 matrix where rows are the three land types (annual crops, permanent crops, and pastureland)
-# and columns are the exchanges ()
+# and columns are the exchanges (for each of the ten BEA codes)
+
+data_to_mat <- function(dat) {
+  mat <- t(as.matrix(dat[,-1]))
+  dimnames(mat)[[2]] <- dat$BEA_code
+  mat
+}
+
+land_exch_tables <- nass_bea %>%
+  select(state_fips, state_abbrev, BEA_code, annual_cropland_exchange, permanent_cropland_exchange, pastureland_exchange) %>%
+  arrange(state_fips, state_abbrev, BEA_code) %>%
+  group_by(state_fips, state_abbrev) %>%
+  nest %>%
+  mutate(land_exchange = map(data, data_to_mat)) %>%
+  select(-data)
+
+# Save to .RData object
+save(land_exch_tables, file = 'data/cfs_io_analysis/state_land_exchange_tables.RData')
--- scenario_analysis_v2/land_footprint_estimate.R
+++ scenario_analysis_v2/land_footprint_estimate.R
@@ -12,45 +12,8 @@ library(tidyverse)
 
 fp_out <- 'data/cfs_io_analysis'
 
-land_exch_imputed <- read_csv(file.path(fp_out, 'land_imputed_exchanges_wide.csv'))
-
-ag_codes <- read_csv('data/crossreference_tables/naics_crosswalk_final.csv') %>%
-  filter(substr(BEA_389_code, 1, 3) %in% c('111', '112')) %>%
-  pull(BEA_389_code)
-
-# Convert land exchanges to a satellite table -----------------------------
-
-# N rows = 3 (subset land exchanges for only annual crops, pasture, and permanent crops)
-# There are only 10 BEA codes accounted for in those land use types. (other crops are split between annual and permanent)
-# So each satellite table will be 3x10 for each state.
-# Then, multiply the appropriate state's satellite table by each county's vector (again subset for the 10 codes)
-
-land_exch_tables <- land_exch_imputed %>%
-  filter(Landuse_type %in% c('Annual crops', 'Permanent crops', 'Pasture')) %>%
-  pivot_longer(-c(Landuse_type, Activity_Name, Code), names_to = 'state', values_to = 'exchange') %>%
-  select(-Activity_Name) %>%
-  group_by(state) %>%
-  pivot_wider(names_from = Landuse_type, values_from = exchange, values_fill = 0) %>% # pivot and unpivot to add the zeroes
-  pivot_longer(-c(state, Code), names_to = 'Landuse_type', values_to = 'exchange') %>%
-  group_by(state, Landuse_type) %>%
-  pivot_wider(names_from = Code, values_from = exchange)
-
-# Get FIPS code of each state
-data(fips_codes,package='tidycensus')
-
-state_fips_table <- unique(fips_codes[,c('state','state_code')]) %>%
-  mutate(state = paste0('US_', state))
-
-# Convert land exchange tables to a list of matrices
-# Ensure the matrix's row names are sorted the same way as the demand vectors.
-land_exch_matrices <- land_exch_tables %>%
-  group_by(state) %>% nest %>%
-  mutate(data = map(data, function(x) {
-    dat <- as.matrix(x[,-1])
-    row.names(dat) <- x$Landuse_type
-    dat[, ag_codes]
-  }))
-
+# Load land_exch_tables
+load(file.path(fp_out, 'state_land_exchange_tables.RData'))
 
 # Loop through scenarios and get land consumption -------------------------
 
@@ -76,32 +39,30 @@ land_consumption_by_scenario <- function(diet, waste) {
   consumption_vectors <- consumption_fromstate %>%
     group_by(scenario, county_fips, state_from) %>%
     nest %>%
-    mutate(data = map(data, ~ setNames(as.numeric(.$consumption), .$BEA_code)))
-  
-  # Get state abbreviation for the fips codes
-  consumption_vectors <- consumption_vectors %>% left_join(state_fips_table, by = c('state_from' = 'state_code'))
+    mutate(consumption = map(data, ~ setNames(as.numeric(.$consumption), .$BEA_code))) %>%
+    select(-data)
   
   # Join consumption vectors with the land exchange table for the appropriate state
   consumption_vectors <- consumption_vectors %>%
-    left_join(land_exch_matrices, by = 'state')
+    left_join(land_exch_tables, by = c('state_from' = 'state_fips'))
   
   # Get rid of null entries for production (DC)
   consumption_vectors <- consumption_vectors %>%
-    filter(!map_lgl(data.y, is.null))
+    filter(!map_lgl(land_exchange, is.null))
   
   # Here, data.x are the total consumption vectors and data.y are the exchange tables
   
   # Function to get properly formatted land consumption for each row
-  get_land_consumption = function(data.x, data.y) {
-    data.x <- data.x[dimnames(data.y)[[2]]]
-    p <- data.y %*% data.x
-    data.frame(land_type = dimnames(data.y)[[1]], land_consumption = p)
+  get_land_consumption = function(consumption, land_exchange) {
+    consumption <- consumption[dimnames(land_exchange)[[2]]] # Ensures both are sorted the same.
+    p <- land_exchange %*% consumption
+    data.frame(land_type = dimnames(land_exchange)[[1]], land_consumption = p)
   }
   
   
   # Do the matrix multiplication for each row to get the land consumption!
   consumption_vectors <- consumption_vectors %>%
-    mutate(land_consumption = map2(data.x, data.y, get_land_consumption))
+    mutate(land_consumption = map2(consumption, land_exchange, get_land_consumption))
   
   # Unnest list column
   land_consumption <- consumption_vectors %>%
@@ -119,7 +80,7 @@ library(rslurm)
 
 sjob <- slurm_apply(land_consumption_by_scenario, scenario_combos, 
                     jobname = 'county_land', nodes = 4, cpus_per_node = 2, 
-                    global_objects = c('land_exch_matrices', 'state_fips_table'),
+                    global_objects = c('land_exch_tables'),
                     slurm_options = list(partition = 'sesync'))
 
 
