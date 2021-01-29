6681cbc19a9dfcd7aac65ee425a8ea23a37aade3
qdread <qread@sesync.org>
2021-01-29 05:20:21 -0500

    update summary data script

--- debug_misc/incompletejobs.R
+++ debug_misc/incompletejobs.R
@@ -6,16 +6,13 @@ fileInfo <- pmap_dfr(scenario_combos, function(diet, waste) glue::glue('data/cfs
 lubridate::day(fileInfo$ctime)
 
 jobstorun <- which(fileInfo$ctime < as.POSIXct("2021-01-20 16:00:00"))
+jobstorun <- which(lubridate::day(fileInfo$ctime) < 27)
 
-sjob2 <- slurm_apply(land_consumption_by_scenario, scenario_combos[jobstorun, ], 
-                    jobname = 'county_land2', nodes = 5, cpus_per_node = 2, 
-                    global_objects = c('land_exch_tables'),
-                    slurm_options = list(partition = 'sesync'))
-
-sjob3 <- slurm_apply(land_consumption_by_scenario, scenario_combos[5, ], 
-                     jobname = 'county_land3', nodes = 1, cpus_per_node = 1, 
-                     global_objects = c('land_exch_tables'),
-                     slurm_options = list(partition = 'sesync'))
+# Run with one job per node to ensure there is adequate memory.
+sjob_extracounties <- slurm_apply(land_consumption_by_scenario, scenario_combos[jobstorun, ], 
+                                  jobname = 'county_landextra', nodes = length(jobstorun), cpus_per_node = 1, 
+                                  global_objects = c('land_exch_tables'),
+                                  slurm_options = list(partition = 'sesync'))
 
 # Read one and see if it is OK.
 x <- read_csv('data/cfs_io_analysis/county_land_consumption_csvs/D_baseline_WR_baseline_landconsumption.csv', n_max = 10)
@@ -25,12 +22,8 @@ fileInfo <- pmap_dfr(scenario_combos, function(diet, waste) glue::glue('data/cfs
 
 jobstorun <- which(fileInfo$ctime < as.POSIXct("2021-01-12 21:00:00"))
 
-sjob_convertflows <- slurm_apply(county_flows_to_tnc_flows, scenario_combos[jobstorun, ], 
-                                 jobname = 'convert_flows', nodes = 1, cpus_per_node = 4, preschedule_cores = FALSE,
-                                 global_objects = c('county_tnc_weights'),
-                                 slurm_options = list(partition = 'sesync'))
+sjob_convertflowsextra <- slurm_apply(county_flows_to_tnc_flows, scenario_combos[jobstorun, ], 
+                                      jobname = 'convert_flowsextra', nodes = 1, cpus_per_node = 4, preschedule_cores = FALSE,
+                                      global_objects = c('county_tnc_weights'),
+                                      slurm_options = list(partition = 'sesync'))
 
-sjob_convertflows <- slurm_apply(county_flows_to_tnc_flows, scenario_combos[5, ], 
-                                 jobname = 'convert_flows', nodes = 1, cpus_per_node = 1, preschedule_cores = FALSE,
-                                 global_objects = c('county_tnc_weights'),
-                                 slurm_options = list(partition = 'sesync'))
--- figs/figs_v2_summarydata_parallel.R
+++ figs/figs_v2_summarydata_parallel.R
@@ -23,11 +23,11 @@ sum_goods_flows_county <- function(diet, waste) {
   # Outbound: each row represents origin county, sum across all destination counties (columns)
   flows_outbound <- cbind(flows[, 1:3], flow_outbound = apply(flows[, -(1:3)], 1, sum)) %>%
     rename(county = county_fips)
-  
+
   # Inbound: exclude column denoting origin county then sum by group and reshape
-  flows_inbound <- flows %>% 
+  flows_inbound <- flows %>%
     select(-county_fips) %>%
-    group_by(BEA_code, scenario) %>% 
+    group_by(BEA_code, scenario) %>%
     summarize_all(sum) %>%
     ungroup %>%
     pivot_longer(-c(BEA_code, scenario), names_to = 'county', values_to = 'flow_inbound')
@@ -36,7 +36,7 @@ sum_goods_flows_county <- function(diet, waste) {
   full_join(flows_inbound, flows_outbound, by = c('scenario', 'BEA_code', 'county')) %>%
     separate(scenario, into = c('D', 'scenario_diet', 'W', 'scenario_waste'), sep = '_') %>%
     select(scenario_diet, scenario_waste, BEA_code, county, flow_inbound, flow_outbound)
- 
+
 }
 
 county_goods_flows <- future_pmap_dfr(scenario_combos, sum_goods_flows_county)
@@ -58,10 +58,10 @@ sum_land_flows_county <- function(diet, waste) {
     rename(county = county_from)
   
   flows_inbound <- flows %>%
-    group_by(scenario, land_type, county_fips) %>%
+    group_by(scenario, land_type, county_to) %>%
     summarize(flow_inbound = sum(land_consumption, na.rm = TRUE)) %>%
     ungroup %>%
-    rename(county = county_fips)
+    rename(county = county_to)
 
   # Join and split scenario column up
   full_join(flows_inbound, flows_outbound, by = c('scenario', 'land_type', 'county')) %>%
