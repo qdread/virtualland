63e87c0812f13aa2913e8a3bcb4c5dcb2e4c36e9
qdread <qread@sesync.org>
2021-05-27 13:31:25 -0400

    Supp table foreign imports of goods, land, biodiv

--- tables/foreign_imports_tables.R
+++ tables/foreign_imports_tables.R
@@ -39,6 +39,9 @@ import_biodiv_base_sums[, prop_VBT := cumsum(species_lost)/sum(species_lost)]
 
 top20biodiv <- import_biodiv_base_sums$country_name[1:20]
 
+# Paste ISO3 code to the end of the country names
+top20biodiv_iso <- import_biodiv$ISO_A3[match(top20biodiv, import_biodiv$country_name)]
+top20biodiv_withcode <- paste0(top20biodiv, ' (', top20biodiv_iso, ')')
 
 # Create land/goods import df ---------------------------------------------
 
@@ -61,11 +64,56 @@ import_goods_base_wide[!country_name %in% top20biodiv, country_name := 'Other']
 cols_to_sum <- c("export_qty", "VLT_annual", "VLT_mixed", "VLT_permanent", "VLT_pasture")
 import_goods_base_wide <- import_goods_base_wide[, lapply(.SD, sum), by = .(country_name, item), .SDcols = cols_to_sum]
 
+# Sum the virtual land transfer for each country x land type
+import_goods_base_wide[, VLT_total := VLT_annual + VLT_pasture + VLT_permanent + VLT_mixed]
+
 # Aggregate biodiversity dataframe to "other"
 import_biodiv_base[!country_name %in% top20biodiv, country_name := 'Other']
 import_biodiv_base <- import_biodiv_base[, .(species_lost = sum(species_lost)), by = .(country_name, land_use, taxon)]
 import_biodiv_base[, land_use := paste0('VBT_', land_use)]
 import_biodiv_base_wide <- dcast(import_biodiv_base, country_name + taxon ~ land_use)
+import_biodiv_base_wide[, VBT_total := VBT_annual + VBT_pasture + VBT_permanent]
 
-# Wideform table of export quantity and virtual land transfer
-import_qty_wide <- dcast(import_crop_base, country_name ~ item, value.var = 'export_qty')
+# Sort descending order of total virtual biodiversity transfer
+import_goods_base_wide[, country_name := factor(country_name, levels = c(top20biodiv, 'Other'), labels = c(top20biodiv_withcode, 'Other'))]
+import_biodiv_base_wide[, country_name := factor(country_name, levels = c(top20biodiv, 'Other'), labels = c(top20biodiv_withcode, 'Other'))]
+import_biodiv_base_wide[, taxon := factor(taxon, levels = c('plants', 'amphibians', 'birds', 'mammals', 'reptiles'))]
+
+import_goods_base_wide <- import_goods_base_wide[order(country_name, -VLT_total), .(country_name, item, export_qty, VLT_total)]
+import_biodiv_base_wide <- import_biodiv_base_wide[order(country_name, -VBT_total), .(country_name, taxon, VBT_total)]
+
+
+# Generate gt tables ------------------------------------------------------
+
+# for virtual land transfer, convert to km^2 (was ha so divide by 100)
+
+library(dplyr)
+
+gt_import_goods_land <- import_goods_base_wide %>%
+  as_tibble %>%
+  mutate(item = tolower(gsub('\xe9', 'é', item)), # for yerba mate
+         VLT_total = signif(VLT_total/100, 3)) %>%
+  gt(groupname_col = 'country_name') %>%
+  fmt_number(c(export_qty, VLT_total), n_sigfig = 3) %>%
+  cols_label(
+    item = 'Item',
+    export_qty = html('Export quantity<br><small><i>tonnes</i></small>'),
+    VLT_total = html('Virtual land export<br><small><i>km<sup>2</sup></i></small>')
+  )
+
+# Make biodiversity table even wider (by taxon)
+import_biodiv_base_wider <- dcast(import_biodiv_base_wide, country_name ~ taxon)
+import_biodiv_base_wider[, animals := amphibians + birds + mammals + reptiles]
+import_biodiv_base_wider[, total := plants + animals]
+
+gt_import_biodiv <- import_biodiv_base_wider %>%
+  gt(rowname_col = 'country_name') %>%
+  fmt_number(2:8, n_sigfig = 3) %>%
+  tab_spanner(
+    label = html("Virtual biodiversity threat export by taxonomic group<br><small><i>unit: extinctions</i></small>"),
+    columns = 2:8
+  ) %>%
+  data_color(columns = 2:8, "Reds", alpha = 0.75)
+
+saveRDS(gt_import_goods_land, 'data/cfs_io_analysis/scenario_v2_figs/gt_tables/gt_import_goods_land.RDS')
+saveRDS(gt_import_biodiv, 'data/cfs_io_analysis/scenario_v2_figs/gt_tables/gt_import_biodiv.RDS')
