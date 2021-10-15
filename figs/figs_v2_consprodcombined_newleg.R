b8b66f96c5a2db74c52e1667cc7ed5fb5849a22b
qdread <qread@sesync.org>
2021-10-15 16:10:29 -0400

    edits to figs for formatting

--- figs/figs_v2_consprodcombined_newleg.R
+++ figs/figs_v2_consprodcombined_newleg.R
@@ -1,12 +1,14 @@
 # Combine diet consumption with scenario production in a single figure (was formerly Figs 1 and 2)
 # data generated in figs_v2_consumption.R
 
-foodgroups_ordered <- c('grain', 'fruit', 'nuts', 'vegetables', 'sugar', 'meat/eggs', 'dairy', 'fish', 'fat')
+lafa_cal_summ <- lafa_cal_summ %>% mutate(food_group = if_else(food_group == 'fat', 'added fats', food_group))
+foodgroups_ordered <- c('grain', 'fruit', 'nuts', 'vegetables', 'sugar', 'meat/eggs', 'dairy', 'fish', 'added fats')
 p_consprod_top <- ggplot(lafa_cal_summ %>% mutate(food_group = factor(food_group, levels = foodgroups_ordered)), aes(y = calories_day, x = food_group, color = diet, fill = diet, group = diet)) +
-  geom_col(position = 'dodge', color = 'black', size = 0.25) +
+  geom_col(position = 'dodge', color = 'black', size = 0.1) +
   scale_fill_manual(values = unname(okabe_colors[c(1,7,3,4,5)]), labels = diet_long_names$medium_name) +
   scale_y_continuous(expand = expansion(mult = c(0, 0.03)), name = 'calories per person per day') +
   scale_x_discrete(name = 'food group') +
+  theme_classic() +
   theme(legend.position = c(0.65, 0.85),
         legend.text = element_text(size = rel(.65)),
         legend.background = element_blank(),
@@ -15,19 +17,21 @@ p_consprod_top <- ggplot(lafa_cal_summ %>% mutate(food_group = factor(food_group
   guides(fill = guide_legend(ncol = 3, title = NULL))
 
 p_consprod_bottom <- ggplot(totaldemand_relative %>% filter(scenario_waste %in% c('baseline', 'allavoidable'))) +
-  geom_col(aes(x = short_name, y = demand, color = scenario_diet, fill = scenario_diet, group = scenario_diet), position = 'dodge', color = 'black', size = 0.25) +
+  geom_col(aes(x = short_name, y = demand, color = scenario_diet, fill = scenario_diet, group = scenario_diet), position = 'dodge', color = 'black', size = 0.1) +
   geom_hline(yintercept = 1, linetype = 'dotted', color = 'black', size = 0.5) +
+  geom_hline(yintercept = 0, color = 'black', size = 0.5) +
   geom_text(data = data.frame(scenario_waste = factor(c('baseline', 'allavoidable'), levels = c('baseline', 'allavoidable')), label = paste('waste scenario:', waste_long_names$long_name[c(1,4)])), aes(label = label), x = Inf, y = Inf, hjust = 1.01, vjust = 1.3) +
   scale_fill_manual(name = 'diet', values = unname(okabe_colors[c(1,7,3,4,5)]), labels = diet_long_names$medium_name) +
   facet_wrap(~ scenario_waste, nrow = 2) +
   scale_x_discrete(name = 'primary agricultural product') +
   scale_y_continuous(expand = expansion(mult = c(0, 0.03)), name = 'production relative to baseline') +
+  theme_classic() +
   theme(axis.text.x = element_text(angle = 60, hjust = 1, size = rel(0.9)),
         legend.position = 'none',
         strip.background = element_blank(),
         strip.text.x = element_blank()) +
   guides(fill=guide_legend(nrow = 2, byrow = FALSE))
 
-png(file.path(fp_fig, 'cons_prod_fig1_combined_legendinbox.png'), height = 9, width = 6, res = 400, units = 'in')
+png(file.path(fp_fig, 'fig1_revision.png'), height = 9, width = 6, res = 400, units = 'in')
 grid.arrange(p_consprod_top, p_consprod_bottom, nrow = 2, heights = c(1, 2.6))
 dev.off()
