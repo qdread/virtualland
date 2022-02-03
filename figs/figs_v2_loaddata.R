3db7b9807db38bde0dae9f742b98e092741c3845
qdread <qread@sesync.org>
2022-02-03 06:31:20 -0500

    reorder import

--- figs/figs_v2_loaddata.R
+++ figs/figs_v2_loaddata.R
@@ -49,10 +49,6 @@ county_extinction_flow_sums <- read_csv('data/cfs_io_analysis/scenarios/species_
 # Flows of species extinctions between states (raw pairwise)
 state_extinction_flows <- read_csv('data/cfs_io_analysis/scenarios/species_lost_state_x_state_all_scenarios_med.csv', col_types = 'cccccd')
 
-# Foreign virtual land transfers into USA, by country x tnc combination exporting them and by county importing them
-foreign_vlt_export <- read_csv('data/cfs_io_analysis/foreign_VLT_by_country_x_TNC.csv')
-foreign_vlt_import <- read_csv('data/cfs_io_analysis/foreign_VLT_to_counties.csv')
-
 # Foreign flows of species extinctions into USA, by country x tnc combination exporting them and by county importing them
 foreign_extinction_export <- read_csv('data/cfs_io_analysis/scenarios/foreign_species_lost_by_export_country_x_tnc.csv')
 foreign_extinction_import <- read_csv('data/cfs_io_analysis/scenarios/foreign_species_lost_by_import_county.csv')
@@ -60,6 +56,10 @@ foreign_extinction_import <- read_csv('data/cfs_io_analysis/scenarios/foreign_sp
 # Foreign goods transfers into USA, one for animals and one for crops
 foreign_animal_export <- read_csv('data/cfs_io_analysis/fao_production_trade_animals.csv')
 foreign_crop_export <- read_csv('data/cfs_io_analysis/fao_production_trade_crops.csv')
+
+# Foreign virtual land transfers into USA, by country x tnc combination exporting them and by county importing them
+foreign_vlt_export <- read_csv('data/cfs_io_analysis/foreign_VLT_by_country_x_TNC.csv')
+foreign_vlt_import <- read_csv('data/cfs_io_analysis/foreign_VLT_to_counties.csv')
 
 # Map of counties in AEA
 county_map <- st_read('data/raw_data/landuse/USA/USA_county_2014_aea.gpkg')
